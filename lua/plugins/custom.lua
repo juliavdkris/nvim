d544431eb77a7707717272ffeb5dde9cff7622f7
Julia van der Kris <juliavdkris@proton.me>
2025-04-13 21:08:49 +0200

    Switch from neotree and fzf-lua to snacks?

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -15,6 +15,21 @@ return {
 			notifier = {
 				top_down = false,
 			},
+			picker = {
+				sources = {
+					explorer = {
+						layout = { layout = { position = "right" } },
+					},
+					keymaps = {
+						layout = {
+							preset = "vertical",
+							layout = {
+								width = 0.6,
+							},
+						},
+					},
+				},
+			},
 		},
 	},
 	{
