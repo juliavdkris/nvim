4b0fa9b535dfa3fa0ad05cacdd0242dd7386d310
Julia van der Kris <juliavdkris@proton.me>
2025-04-13 19:39:20 +0200

    Disable Obsidian plugin automatically inserting frontmatter on save

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -14,8 +14,6 @@ return {
 	dependencies = {
 		-- Required.
 		"nvim-lua/plenary.nvim",
-
-		-- see below for full list of optional dependencies 👇
 	},
 	opts = {
 		workspaces = {
@@ -24,7 +22,6 @@ return {
 				path = "~/Documents/notes",
 			},
 		},
-
-		-- see below for full list of options 👇
+		disable_frontmatter = true,
 	},
 }
