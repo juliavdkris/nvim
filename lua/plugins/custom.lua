e9fe32eaec5f7737aea6a59646ecc4bcdd321794
Julia van der Kris <juliavdkris@proton.me>
2025-09-08 23:14:13 +0200

    Bufferline workaround
    https://github.com/LazyVim/LazyVim/issues/6355
    https://github.com/LazyVim/LazyVim/pull/6354

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -28,4 +28,13 @@ return {
 			colorscheme = "catppuccin",
 		},
 	},
+	{
+		"akinsho/bufferline.nvim",
+		init = function()
+			local bufline = require("catppuccin.groups.integrations.bufferline")
+			function bufline.get()
+				return bufline.get_theme()
+			end
+		end,
+	},
 }
